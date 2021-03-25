ff6788bdd565ee658501e8a108d0b0134b2eed36
Andrea Macchieraldo <a.macchieraldo@gmail.com>
2021-03-25 11:24:02 +0100

    fix all

--- scripts/snx.sh
+++ scripts/snx.sh
@@ -23,30 +23,25 @@
 server=$SNX_SERVER
 user=$SNX_USER
 password=$SNX_PASSWORD
-snx_command=""
 certificate_path="/certificate.p12"
 
 if [ -f "$certificate_path" ]; then
     if [ ! -z "$user" ]; then
-        snx_command="snx -s $server -u $user -c $certificate_path"
+        echo -e "$SNX_PASSWORD\ny" | snx -s $server -u $user -c $certificate_path
     else
-        snx_command="snx -s $server -c $certificate_path"
+        echo -e "$SNX_PASSWORD\ny" | snx -s $server -c $certificate_path
     fi
 else
-    snx_command="snx -s $server -u $user"
+    echo -e "$SNX_PASSWORD\ny" | snx -s $server -u $user
 fi
+
+# alt method
+# snx -s $server -c $certificate_path << 'EOF'
+# mypass
+# y
+# EOF
 
 iptables -t nat -A POSTROUTING -o tunsnx -j MASQUERADE
 iptables -A FORWARD -i eth0 -j ACCEPT
-
-/usr/bin/expect <<EOF
-spawn $snx_command
-expect "*?assword:"
-send "$password\r"
-expect "*Do you accept*"
-send "y\r"
-expect "SNX - connected."
-interact
-EOF
 
 /bin/bash
